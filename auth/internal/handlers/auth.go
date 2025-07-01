d2cd18b72efb6ddd5495b26d0cd0ea802bed7c0c
c0rrupt3dlance <letamm.sob@gmail.com>
2025-07-01 18:00:41 +0500

    feat: added signup handler

--- auth/internal/handlers/auth.go
+++ auth/internal/handlers/auth.go
@@ -1,1 +1,24 @@
 package handlers
+
+import (
+	"github.com/c0rrupt3dlance/web-pharma-store/auth/internal/models"
+	"github.com/gin-gonic/gin"
+	"net/http"
+)
+
+func (h *Handler) SignUp(c *gin.Context) {
+	var user models.User
+
+	if err := c.ShouldBindJSON(&user); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	userId, err := h.services.Create(user)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+
+	c.JSON(http.StatusCreated, gin.H{"user_id": userId})
+}
--- auth/internal/models/user.go
+++ auth/internal/models/user.go
@@ -2,10 +2,10 @@ package models
 
 type User struct {
 	Id       int    `json:"id"`
-	Name     string `json:"name"`
-	Surname  string `json:"surname"`
-	Username string `json:"username"`
-	Email    string `json:"email"`
-	Password string `json:"password"`
+	Name     string `json:"name" binding:"required"`
+	Surname  string `json:"surname" binding:"required"`
+	Username string `json:"username" binding:"required"`
+	Email    string `json:"email" binding:"required"`
+	Password string `json:"password" binding:"required"`
 	IsAdmin  bool   `json:"is_admin"`
 }
