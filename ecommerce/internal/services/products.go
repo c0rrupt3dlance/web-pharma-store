d2edf755cbd8c0660e40bdec247d43d943c1fae8
letamm <letamm.sob@gmail.com>
2025-07-09 23:35:20 +0500

    ecommerce feat: added products service

---
+++ ecommerce/internal/services/products.go
@@ -0,0 +1,29 @@
+package services
+
+import (
+	"github.com/c0rrupt3dlance/web-pharma-store/ecommerce/internal/models"
+	"github.com/c0rrupt3dlance/web-pharma-store/ecommerce/internal/repository"
+)
+
+type ProductsService struct {
+	repo repository.Products
+}
+
+func NewProductsService(repo *repository.Repository) *ProductsService {
+	return &ProductsService{
+		repo: repo,
+	}
+}
+
+func (s *ProductsService) Create(product models.Product) (int, error) {
+	return s.repo.Create(product)
+}
+func (s *ProductsService) GetById(ProductId int) (models.ProductResponse, error) {
+	return s.repo.GetById(ProductId)
+}
+func (s *ProductsService) Update(product models.Product) error {
+	return s.repo.Update(product)
+}
+func (s *ProductsService) Delete(ProductId int) error {
+	return s.repo.Delete(ProductId)
+}
--- ecommerce/internal/services/service.go
+++ ecommerce/internal/services/service.go
@@ -1,28 +1,23 @@
 package services
 
 import (
-	"github.com/c0rrupt3dlance/web-pharma-store/auth/internal/models"
-	"github.com/c0rrupt3dlance/web-pharma-store/auth/internal/repository"
+	"github.com/c0rrupt3dlance/web-pharma-store/ecommerce/internal/models"
+	"github.com/c0rrupt3dlance/web-pharma-store/ecommerce/internal/repository"
 )
 
-type Authorization interface {
-	Create(user models.User) (int, error)
-	GenerateTokens(username, password string) (string, string, error)
-	RefreshTokens(refreshToken string) (string, string, error)
-	VerifyAccessToken(tokenString string) (models.User, error)
+type Products interface {
+	Create(product models.Product) (int, error)
+	GetById(ProductId int) (models.ProductResponse, error)
+	Update(product models.Product) error
+	Delete(ProductId int) error
 }
 
 type Service struct {
-	Authorization
+	Products
 }
 
-type Dependencies struct {
-	Repo       *repository.Repository
-	SigningKey string
-}
-
-func NewService(repo *repository.Repository, signingKey string) *Service {
+func NewService(repo *repository.Repository) *Service {
 	return &Service{
-		NewAuthService(repo, signingKey),
+		Products: NewService(repo),
 	}
 }
